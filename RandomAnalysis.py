71da4bfcb03556ec57e4de785d8f681f0c4656a3
Pepper-13 <pepper_1317@yahoo.com>
2017-09-25 23:49:54 +0530

    Update RandomAnalysis.py

--- RandomAnalysis.py
+++ RandomAnalysis.py
@@ -58,5 +58,19 @@ def dict_results(results_excel):
           
     #Uncomment to check out one
     #print results[results.keys()[0]]
+    
+    return results
+          
+def shaperead(file_shape):
+    '''Reads data from a shape file and returns some vertices.'''
+    sf = shapefile.Reader(file_shape)
+    all_verts = []
+    
+    #Grabs all the vertices
+    for shape in sf.shapes():
+        all_verts.append([(point[0],point[1]) for point in shape.points])        
+    identifiers = [rec[2] for rec in sf.records()]
+    
+    return (all_verts, identifiers)
 
 
