c5f1c08566345a8a55095d61e6364e8ef479c9b1
Pepper-13 <pepper_1317@yahoo.com>
2017-09-25 23:49:33 +0530

    Update RandomAnalysis.py

--- RandomAnalysis.py
+++ RandomAnalysis.py
@@ -32,3 +32,31 @@ data_in = {
         }
     }
 
+def dict_results(results_excel):
+    '''Gives you a results dictionary for a given excel doc.  The dictionary has entries which are dictionaries themselves - one for each subdivision in the file.'''
+    workbook = xlrd.open_workbook(results_excel)
+    worksheets = workbook.sheet_names()
+    results = {}
+
+    for worksheet_name in worksheets:
+        worksheet = workbook.sheet_by_name(worksheet_name)
+
+        #Loop through all of the columns other than the first and last
+        for col in range(1, worksheet.ncols - 2):
+            #I think this subdivision tab is only in 2006.\n,
+            if worksheet.cell_value(1,col) != 'Subdivision':
+                #Initialize the subdivision dictionary
+                dic = {}
+                identifier = worksheet_name[4::].zfill(2) + str(int(worksheet.cell_value(1, col))).zfill(3)
+                #Loop through all candidates
+                for r in range(2, worksheet.nrows):
+                    if r < worksheet.nrows - 1:
+                         dic[worksheet.cell_value(r, 0)] = worksheet.cell_value(r, col)
+                    else:
+                         dic['ALLCANDIDATES'] = worksheet.cell_value(r, col)
+                results[identifier] = dic
+          
+    #Uncomment to check out one
+    #print results[results.keys()[0]]
+
+
