6f17204130d70fbc76788f9636c08480cd6f8e6a
Pepper-13 <pepper_1317@yahoo.com>
2017-09-26 23:30:10 +0530

    Update Face_Classification.py

--- Face_Classification.py
+++ Face_Classification.py
@@ -7,4 +7,15 @@ class DataLoader(object):
     """Class for loading fer2013 emotion classification dataset or
         imdb gender classification dataset."""
     def __init__(self, dataset_name='imdb', dataset_path=None):
+        self.dataset_name = dataset_name
+        self.dataset_path = dataset_path
+        if self.dataset_path != None:
+            self.dataset_path = dataset_path
+        elif self.dataset_name == 'imdb':
+            self.dataset_path = '../datasets/imdb_crop/imdb.mat'
+        elif self.dataset_name == 'fer2013':
+            self.dataset_path = '../datasets/fer2013/fer2013.csv'
+        else:
+            raise Exception('Incorrect dataset name, please input imdb or fer2013')
+
     
