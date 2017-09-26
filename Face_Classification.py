91dff9186aece07df17e088e2fc68417f55b32f6
Pepper-13 <pepper_1317@yahoo.com>
2017-09-26 23:31:03 +0530

    Update Face_Classification.py

--- Face_Classification.py
+++ Face_Classification.py
@@ -24,5 +24,26 @@ class DataLoader(object):
         elif self.dataset_name == 'fer2013':
             ground_truth_data = self._load_fer2013()
         return ground_truth_data
+    
+    
+    def _load_imdb(self):
+        face_score_treshold = 3
+        dataset = loadmat(self.dataset_path)
+        image_names_array = dataset['imdb']['full_path'][0, 0][0]
+        gender_classes = dataset['imdb']['gender'][0, 0][0]
+        face_score = dataset['imdb']['face_score'][0, 0][0]
+        second_face_score = dataset['imdb']['second_face_score'][0, 0][0]
+        face_score_mask = face_score > face_score_treshold
+        second_face_score_mask = np.isnan(second_face_score)
+        unknown_gender_mask = np.logical_not(np.isnan(gender_classes))
+        mask = np.logical_and(face_score_mask, second_face_score_mask)
+        mask = np.logical_and(mask, unknown_gender_mask)
+        image_names_array = image_names_array[mask]
+        gender_classes = gender_classes[mask].tolist()
+        image_names = []
+        for image_name_arg in range(image_names_array.shape[0]):
+            image_name = image_names_array[image_name_arg][0]
+            image_names.append(image_name)
+        return dict(zip(image_names, gender_classes))
 
     
